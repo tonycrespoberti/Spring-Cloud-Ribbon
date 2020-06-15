4c97fa69363ab4bfd6573b305e6a8c4698104fae
Tony Crespo <57902656+tonycrespoberti@users.noreply.github.com>
2020-06-15 12:56:58 +0200

    Add files via upload

---
+++ Spring-Cloud-Netflix-Products/src/main/java/com/cloud/netflix/products/SpringCloudNetflixProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.netflix.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNetflixProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudNetflixProjectApplication.class, args);
+	}
+
+}
